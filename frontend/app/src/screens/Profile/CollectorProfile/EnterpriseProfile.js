da97cc13b452d106f0bc09f1356c2692bd1a2a65
Patrick Salgado <patricksalgado16@gmail.com>
2025-11-08 21:18:46 -0300

    Alteração Requisição

--- frontend/app/src/screens/Profile/CollectorProfile/EnterpriseProfile.js
+++ frontend/app/src/screens/Profile/CollectorProfile/EnterpriseProfile.js
@@ -31,7 +31,7 @@ export default function EnterpriseProfile({ navigation }) {
 
     const fetchEnterpriseData = async () => {
       try {
-        const res = await axios.get(`https://subattenuated-epithetically-eryn.ngrok-free.dev/api/collector/${idCollector}`);
+        const res = await axios.get(`https://2a600c282efc.ngrok-free.app/api/collector/${idCollector}`);
         const data = Array.isArray(res.data) ? res.data[0] : res.data;
         setNameEnterprise(data.nameEnterprise || '');
         setCnpj(formatCnpj(data.cnpj || ''));
@@ -71,7 +71,7 @@ export default function EnterpriseProfile({ navigation }) {
     setIsLoading(true);
     try {
       await axios.put(
-        `https://subattenuated-epithetically-eryn.ngrok-free.dev/api/collector/${idCollector}`,
+        `https://2a600c282efc.ngrok-free.app/api/collector/${idCollector}`,
         {
           nameEnterprise,
           cnpj: unformatCnpj(cnpj),
